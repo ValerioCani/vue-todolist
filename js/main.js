ba6705904fa9c1f08efb502b2509a4d4a55da1b6
Valerio <canivalerio93@gmail.com>
2022-07-25 17:56:40 +0200

    manca il primo bonus

--- js/main.js
+++ js/main.js
@@ -1,9 +1,69 @@
 var app = new Vue({
     el: '#app',
     data:{
-    
+        NewDaFare: "", 
+        lista:[
+            {
+                text:'finire gli esercizi',
+                done:'false'
+            },
+            {
+                text:'allenarmi',
+                done:'false'
+            },
+            {
+                text:'finire gli esercizi',
+                done:'false'
+            },
+            {
+                text:'allenarmi',
+                done:'false'
+            },
+            {
+                text:'finire gli esercizi',
+                done:'false'
+            },
+            {
+                text:'allenarmi',
+                done:'false'
+            },
+            {
+                text:'finire gli esercizi',
+                done:'false'
+            },
+            {
+                text:'allenarmi',
+                done:'false'
+            },
+        ]
     },
     methods:{
-    
-    }
+        aggiungi(){
+            if(!this.NewDaFare==""){
+                let newobject = {
+                                    text: this.NewDaFare,
+                                    done:'false'
+                                };
+                this.lista.push(newobject);
+                this.NewDaFare="";
+            };
+        },
+
+        cancella(number){
+            this.lista.splice(number,1);
+        },
+
+        giaFatto(number){
+            if(this.done==false){
+                this.done=true;
+                let line = document.querySelectorAll('li');
+                line[number].classList.add('done')
+            }else{
+                this.done=false;
+                let line = document.querySelectorAll('li');
+                line[number].classList.remove('done')
+            }
+        },
+    },
+   
 })
